17cf0ed4e51d1c79c5ad1adf27648a93843225da
Madeleine Goertz <madiemoxie@outlook.com>
2020-03-19 17:07:07 -0700

    Refactored

--- NonogramClient.java
+++ NonogramClient.java
@@ -2,24 +2,25 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-import com.opencsv.exceptions.CsvException;
-
 public class NonogramClient {
-    public static void main(String[] args) throws IOException, CsvException {
-        // this is the answer we're trying to generate
-        Nonogram heart = new Nonogram("heart", new File("lib\\heartRowClues.txt"), new File("lib\\heartColClues.txt"));
-        // heart.solve();
-        Nonogram tenByTen = new Nonogram("10by10", new File("lib\\10by10RowClues.txt"), new File("lib\\10by10ColClues.txt"));
-        // tenByTen.solve();
-        Nonogram fifteenByFifteen = new Nonogram("15by15", new File("lib\\15by15RowClues.txt"), new File("lib\\15by15ColClues.txt"));
-        // fifteenByFifteen.solve();
-        Nonogram twentyByTwenty = new Nonogram("20by20", new File("lib\\20by20RowClues.txt"), new File("lib\\20by20ColClues.txt"));
-        // twentyByTwenty.solve();
-        Nonogram twentyFiveByTwentyFive = new Nonogram("25by25", new File("lib\\25by25RowClues.txt"), new File("lib\\25by25ColClues.txt"));
-        twentyFiveByTwentyFive.solve();
+    public static void main(String[] args) throws IOException {
+        String[] testNames = {"heart",
+                              "10by10",
+                              "15by15",
+                              "20by20",
+                              "25by25"};
+        NonogramSolver[] tests  = new NonogramSolver[testNames.length];
+        for (int i = 0; i < tests.length; i++) {
+            String name = testNames[i];
+            NonogramPuzzle puzzle = new NonogramPuzzle(name, 
+                                new File("lib\\" + name + "RowClues.txt"), 
+                                new File("lib\\" + name + "ColClues.txt"));
+            tests[i] = new NonogramSolver(puzzle);
+            tests[i].solve();
+        }
     }
 
-    private static void manualTurn(Nonogram nonogram) {
+    private static void manualTurn(NonogramPuzzle nonogram) {
         Scanner s = new Scanner(System.in);
         while (true) {
             System.out.println(nonogram);
@@ -27,8 +28,8 @@ public class NonogramClient {
             int r = Integer.parseInt(s.next());
             int c = Integer.parseInt(s.next());
             boolean fill = Character.toLowerCase(s.next().charAt(0)) != 'b';
-            if (fill) nonogram.fill(r, c);
-            else nonogram.blank(r, c);
+            if (fill) nonogram.manualFill(r, c);
+            else nonogram.manualBlank(r, c);
         }
     }
     
